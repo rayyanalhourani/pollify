f58a0de7682fed360c238550b46e75a88fa01944
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-09-17 12:08:35 +0300

    Create layout and index page

---
+++ views/index.view.php
@@ -0,0 +1,12 @@
+<?php
+view("partials/nav.view.php");
+view("partials/header.view.php");
+view("partials/banner.view.php");
+?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    </div>
+</main>
+
+<?php view("partials/footer.view.php"); ?>
