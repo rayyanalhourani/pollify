69bd89ba4fd14351f7f3153e62e995acb97c5eec
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-09-18 15:15:35 +0300

    config db

--- bootstrap.php
+++ bootstrap.php
@@ -9,7 +9,7 @@ $container = new Container();
 $container->bind('Core\Database', function () {
     $config = require base_path('config.php');
 
-    return new Database($config['database']);
+    return new Database($config['database'],$config['database']["name"],$config['database']['password']);
 });
 
 App::setContainer($container);
---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => '',
+        'port' => '',
+        'dbname' => '',
+        'charset' => 'utf8mb4',
+        'user' => "",
+        'password' => ""
+    ],
+];
