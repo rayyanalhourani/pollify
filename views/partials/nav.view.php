7f1db8606d588afae2f8bc61abb6e07e930d4080
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-10-02 11:40:47 +0300

    upload logo

--- views/partials/nav.view.php
+++ views/partials/nav.view.php
@@ -2,8 +2,8 @@
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
-                <div class="flex-shrink-0">
-                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                <div class="">
+                    <img class="h-12 w-12" src='/images/logo2cut.png'
                         alt="Your Company">
                 </div>
                 <div class="hidden md:block">
